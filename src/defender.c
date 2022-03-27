81094ea30b735cc2531388ca3c9831311c9d2dad
ArthurPilone <art.pilone@gmail.com>
2022-03-26 22:19:05 -0300

    Add defender strategy
    Signed-off-by: ArthurPilone <art.pilone@gmail.com>

--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -12,6 +14,17 @@
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
+#define  NULL_POSITION         (position_t)  {-1, -1}
+
+#define  MAX_SAVED_POSITIONS 50
+
+// Internal Functions Declaration
+void initialize_position_array(position_t*, int);
+
+short position_array_contains(position_t, position_t*, int);
+
+short add_position_to_array(position_t x, position_t*, int, int);
+
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
@@ -19,11 +32,40 @@
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
   UNUSED(attacker_spy);
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
-}
+  // State Variables
+  static int tick = 0;
+  static short jiggling = 1;
 
-/*----------------------------------------------------------------------------*/
+  static size_t attacker_i;
+
+  direction_t nextDir; 
+
+  if(tick == 0){
+    srand((unsigned) 151115 + time(NULL));
+  }else if(tick == 3){
+    attacker_i = get_spy_position(attacker_spy).i;
+    jiggling = 0;
+  }
+
+  if(jiggling){
+    if(rand() % 2){
+      nextDir = (direction_t) DIR_UP;
+    }else{
+      nextDir = (direction_t) DIR_DOWN;
+    }
+  }else{
+    if(attacker_i > defender_position.i){
+      nextDir = (direction_t) DIR_DOWN;
+    }else if(attacker_i < defender_position.i){
+      nextDir = (direction_t) DIR_UP;
+    }else{
+      jiggling = 1;
+      nextDir = (direction_t) DIR_STAY;
+    }
+  }
+  
+  tick++;
+  return nextDir;
+}
